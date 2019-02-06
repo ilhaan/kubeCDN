441496b5bbdbde2108dc9492b2ba3cb897baaa8d
Ilhaan Rasheed <ilhaan@ilhaanrasheed.com>
2019-02-05 22:05:17 -0800

    Update to use full command instead of ohmyzsh alias

--- prometheus/setup_monitoring.sh
+++ prometheus/setup_monitoring.sh
@@ -3,11 +3,11 @@
 set -e
 
 # East
-k create -f prometheus-storageclass.yaml --kubeconfig=../terraform/east-kubeconfig
+kubectl create -f prometheus-storageclass.yaml --kubeconfig=../terraform/east-kubeconfig
 helm install -f prometheus-values.yaml stable/prometheus --name prometheus --namespace prometheus --kubeconfig=../terraform/east-kubeconfig
 helm install -f grafana-values.yaml stable/grafana --name grafana --namespace grafana --kubeconfig=../terraform/east-kubeconfig
 
 # West
-k create -f prometheus-storageclass.yaml --kubeconfig=../terraform/west-kubeconfig
+kubectl create -f prometheus-storageclass.yaml --kubeconfig=../terraform/west-kubeconfig
 helm install -f prometheus-values.yaml stable/prometheus --name prometheus --namespace prometheus --kubeconfig=../terraform/west-kubeconfig
 helm install -f grafana-values.yaml stable/grafana --name grafana --namespace grafana --kubeconfig=../terraform/west-kubeconfig
