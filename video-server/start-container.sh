efe5722242f119204afeff620d65ea0d3b24cf8d
Ilhaan Rasheed <ilhaan@ilhaanrasheed.com>
2019-02-26 17:02:03 -0800

    Add description of script in header

--- video-server/start-container.sh
+++ video-server/start-container.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash 
+#
+# Use this script to build and run the video-server docker 
+# image locally from the Dockerfile in this directory 
+#
+
 docker build -t ilhaanvideo . && docker run -d -p 1935:1935 --name testilhaanvideo ilhaanvideo
